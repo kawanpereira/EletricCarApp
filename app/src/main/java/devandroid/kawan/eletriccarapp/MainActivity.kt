fa8e5307a467d32e2953229f886b40008d8be720
kawanpereira <kawan_cross1@hotmail.com>
2023-07-27 15:46:52 -0300

    Terceiro Commit do App, recuperando valores.

--- app/src/main/java/devandroid/kawan/eletriccarapp/MainActivity.kt
+++ app/src/main/java/devandroid/kawan/eletriccarapp/MainActivity.kt
@@ -2,10 +2,31 @@ package devandroid.kawan.eletriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var preco: EditText
+    lateinit var btnCalcular: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+        setupListeners()
+
+    }
+
+    fun setupView() {
+        preco = findViewById(R.id.et_preco_kwh)
+        btnCalcular = findViewById(R.id.btn_calcular)
+    }
+
+    fun setupListeners() {
+        btnCalcular.setOnClickListener {
+            val textoDigitado = preco.text.toString()
+            Log.d("Texto digitado  ->", textoDigitado)
+
+        }
     }
 }
